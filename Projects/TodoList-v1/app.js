70c045dde3e8a2df98a5f5d6f373858a18267f91
marovichn <nikolamarovic.11@gmail.com>
2023-04-27 00:59:36 +0200

    EJS

--- Projects/TodoList-v1/app.js
+++ Projects/TodoList-v1/app.js
@@ -2,15 +2,36 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
   var today = new Date();
-  var day = today.getDay();
-  if (day === 6 || day === 0) {
-    res.send("Yay its weekend!!");
-  } else {
-    res.sendFile(__dirname + "/index.html");
+  var dayIndex = /* today.getDay() */ Math.round(Math.random() * 0.6 * 10);
+  console.log(dayIndex);
+  let day = "";
+
+  switch (dayIndex) {
+    case 1:
+      day = "Monday";
+    case 3:
+      day = "Tuesday";
+    case 4:
+      day = "Wednesday";
+    case 2:
+      day = "Thursday";
+    case 5:
+      day = "Friday";
+    case 6:
+      day = "Saturday";
+    case 0:
+      day = "Sunday";
+
+      break;
+    default:
+      console.log("ERROR");
   }
+
+  res.render("list.ejs", { kindOfDay: day });
 });
 
 app.listen(3000, function () {
