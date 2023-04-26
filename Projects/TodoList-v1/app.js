419e1377437c8f5b2c524ce37f00e22006350205
marovichn <nikolamarovic.11@gmail.com>
2023-04-27 01:41:12 +0200

    EJS-dynamic-expr.

--- Projects/TodoList-v1/app.js
+++ Projects/TodoList-v1/app.js
@@ -2,38 +2,28 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+var todoItem = ["Eat food"];
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   var today = new Date();
-  var dayIndex = /* today.getDay() */ Math.round(Math.random() * 0.6 * 10);
-  console.log(dayIndex);
-  let day = "";
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  day = today.toLocaleDateString("en-US", options);
 
-  switch (dayIndex) {
-    case 1:
-      day = "Monday";
-    case 3:
-      day = "Tuesday";
-    case 4:
-      day = "Wednesday";
-    case 2:
-      day = "Thursday";
-    case 5:
-      day = "Friday";
-    case 6:
-      day = "Saturday";
-    case 0:
-      day = "Sunday";
-
-      break;
-    default:
-      console.log("ERROR");
-  }
-
-  res.render("list.ejs", { kindOfDay: day });
+  res.render("list.ejs", { kindOfDay: day, addedToDos: todoItem });
 });
 
 app.listen(3000, function () {
   console.log("Server started on port 3000.");
+});
+
+app.post("/", (req, res) => {
+  todoItem.push(req.body.newItem);
+  res.redirect("/");
 });
