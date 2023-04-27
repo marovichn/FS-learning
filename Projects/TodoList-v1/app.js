b5bc532681f9eaa383c9d247efac6077e315f971
marovichn <nikolamarovic.11@gmail.com>
2023-04-27 02:13:10 +0200

    styling

--- Projects/TodoList-v1/app.js
+++ Projects/TodoList-v1/app.js
@@ -2,14 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-var todoItem = ["Eat food"];
+let todoItem = [];
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  var today = new Date();
-  var options = {
+  let today = new Date();
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long",
