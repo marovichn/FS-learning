b29be487098a6400f7b20eca44a339006fc9eadc
marovichn <nikolamarovic.11@gmail.com>
2023-04-27 02:50:20 +0200

    conditional-saving

--- Projects/TodoList-v1/app.js
+++ Projects/TodoList-v1/app.js
@@ -22,8 +22,22 @@ app.get("/", function (req, res) {
   res.render("list.ejs", { title: day, addedToDos: todoItems });
 });
 
+app.post("/", (req, res) => {
+  let item = req.body.newItem;
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    todoItems.push(item);
+    res.redirect("/");
+  }
+});
+
 app.get("/work", function (req, res) {
-  res.render("list.ejs", { title: "Work", addedToDos: workItems });
+  res.render("list.ejs", {
+    title: "Work",
+    addedToDos: workItems,
+  });
 });
 
 app.post("/work", function (req, res) {
@@ -34,9 +48,4 @@ app.post("/work", function (req, res) {
 
 app.listen(3000, function () {
   console.log("Server started on port 3000.");
-});
-
-app.post("/", (req, res) => {
-  todoItems.push(req.body.newItem);
-  res.redirect("/");
 });
