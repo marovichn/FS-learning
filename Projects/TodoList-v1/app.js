0599805620a042b79079b2d4460b1f845ad9bd91
marovichn <nikolamarovic.11@gmail.com>
2023-04-28 20:55:19 +0200

    modules

--- Projects/TodoList-v1/app.js
+++ Projects/TodoList-v1/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 let day = "";
@@ -11,13 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-  day = today.toLocaleDateString("en-US", options);
+  const day = date();
 
   res.render("list.ejs", { title: day, addedToDos: todoItems });
 });
