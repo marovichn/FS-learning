1982b65ff99b8d3b647b96daaa77bfea0a7ea25e
marovichn <nikolamarovic.11@gmail.com>
2023-04-28 21:32:02 +0200

    linking-adding-more-pages

--- Projects/NickiBlog/app.js
+++ Projects/NickiBlog/app.js
@@ -15,13 +15,23 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
-
 app.get("/",(req,res)=>{
   res.render("home",{
     startingContent: homeStartingContent,
   })
 })
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    startingContent: aboutContent,
+  });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", {
+    startingContent: contactContent,
+  });
+});
 
 
 
