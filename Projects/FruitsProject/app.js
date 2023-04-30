c458062291138d94ea049a5cc34c156a04afc297
marovichn <nikolamarovic.11@gmail.com>
2023-05-01 00:14:35 +0200

    driver-connection

--- Projects/FruitsProject/app.js
+++ Projects/FruitsProject/app.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const app = express();
+
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
 
-const app = express();
+const url = "mongodb://localhost:27017";
 
-app.get("/",()=>{});
+const dbName = "fruitsDB";
 
-app.listen(3000, ()=>{
-    console.log("Server running on port 3000")
-})
+const client = new MongoClient(url);
+
+client.connect((err) => {
+  assert.equal(null, err);
+  console.log("Connected succesfully");
+
+  const db = client.db(dbName);
+
+  client.close();
+});
+
+app.get("/", () => {
+  
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
