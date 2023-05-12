35eb7689b98d73d8df9612d05ca660d3574c498e
marovichn <nikolamarovic.11@gmail.com>
2023-05-12 23:00:52 +0200

    relations-mongoose

--- Projects/FruitsProject/app.js
+++ Projects/FruitsProject/app.js
@@ -14,28 +14,48 @@ async function run() {
 
     const Fruit = mongoose.model("Fruit", fruitSchema);
 
-    const fruit = new Fruit({
-      name: "Apple",
-      rating: 10,
-      review: "Fenomenal"
-    });
-
-    //fruit.save();
-
-    const peopleSchema = mongoose.Schema({
-      name: String,
-      age: Number,
+    const peopleSchema = new mongoose.Schema({
+      name: {
+        type: String,
+        required: true,
+      },
+      age: {
+        type: Number,
+        min: 18,
+        max: 65,
+      },
+      favoriteFruit: fruitSchema,
     })
 
     const People = mongoose.model("People", peopleSchema);
+    
+    const Pineapple = new Fruit({
+      name: "Pineapple",
+      rating: 9,
+      review: "Fenomenallll"
+    });
 
-    const people1 = new People({
+    const person1 = new People({
       name: "Joe",
-      age: 32,
+      age: 18,
+      favoriteFruit: Pineapple,
     })
 
-    people1.save();
+    /* Pineapple.save();
+    person1.save(); */
 
+
+   /* await People.insertMany([people1, people2]); */
+/* 
+   await People.deleteOne({ _id: "645d7432a756b819e21e13df" });
+
+   await People.updateOne({ _id: "645ea43ce22548c03c7e8052" }, { age: 19 }); */
+
+
+
+  /*  console.log(await People.find()); */
+
+   
   } catch (err) {
     console.log(err);
   }
