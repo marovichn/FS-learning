06ac3f0090a975ffde55766948e075224b29ca8b
marovichn <nikolamarovic.11@gmail.com>
2023-05-17 01:29:27 +0200

    api-delete

--- Projects/Wiki-APP/app.js
+++ Projects/Wiki-APP/app.js
@@ -23,75 +23,108 @@ const articlesSchema = new mongoose.Schema({
 
 const Article = new mongoose.model("Article", articlesSchema);
 
-app.route("/articles").get((req, res) => {
-  try {
-    const run = async () => {
-      const articles = await Article.find();
-      res.send(articles);
-    };
-    run().catch((err) => res.send(err));
-  } catch (err) {
-    res.send(err);
-  }
-}).post((req, res) => {
- 
-  try {
-    const run = async () => {
-       const newArticle = new Article({
-         title: req.body.title,
-         content: req.body.content,
-       });
-       await newArticle.save();
-       res.send("Success");
-    };
-    run().catch((err) => res.send(err));
-  } catch (err) {
-    res.send(err);
-  }
-
-}).delete((req,res)=>{
+app
+  .route("/articles")
+  .get((req, res) => {
     try {
-        const run = async () => {
-          await Article.deleteMany();
-          res.send("Success");
-        };
-        run().catch((err) => res.send(err));
+      const run = async () => {
+        const articles = await Article.find();
+        res.send(articles);
+      };
+      run().catch((err) => res.send(err));
     } catch (err) {
       res.send(err);
     }
-});
-
-app.route("/articles/:articleName").get((req,res)=>{
+  })
+  .post((req, res) => {
     try {
-      const articleName = req.params.articleName; 
       const run = async () => {
-        const article = await Article.findOne({title: articleName });
+        const newArticle = new Article({
+          title: req.body.title,
+          content: req.body.content,
+        });
+        await newArticle.save();
+        res.send("Success");
+      };
+      run().catch((err) => res.send(err));
+    } catch (err) {
+      res.send(err);
+    }
+  })
+  .delete((req, res) => {
+    try {
+      const run = async () => {
+        await Article.deleteMany();
+        res.send("Success");
+      };
+      run().catch((err) => res.send(err));
+    } catch (err) {
+      res.send(err);
+    }
+  });
+
+app
+  .route("/articles/:articleName")
+  .get((req, res) => {
+    try {
+      const articleName = req.params.articleName;
+      const run = async () => {
+        const article = await Article.findOne({ title: articleName });
         res.send(article);
       };
       run().catch((err) => res.send(err));
     } catch (err) {
       res.send(err);
     }
-}).put((req,res)=>{
+  })
+  .put((req, res) => {
     try {
       const run = async () => {
-       await Article.findOneAndUpdate(
-         { title: req.params.articleName },
-         {
-           title: req.body.title,
-           content: req.body.content,
-         },{
-            overwrite: true
-         }
-       );
+        await Article.findOneAndUpdate(
+          { title: req.params.articleName },
+          {
+            title: req.body.title,
+            content: req.body.content,
+          },
+          {
+            overwrite: true,
+          }
+        );
         res.send("Success!");
       };
       run().catch((err) => res.send(err));
     } catch (err) {
       res.send(err);
     }
-})
-
+  })
+  .patch((req, res) => {
+    try {
+      const run = async () => {
+        await Article.findOneAndUpdate(
+          { title: req.params.articleName },
+          {
+            $set: req.body,
+          }
+        );
+        res.send("Success!");
+      };
+      run().catch((err) => res.send(err));
+    } catch (err) {
+      res.send(err);
+    }
+  }).delete((req,res)=>{
+    try {
+      const run = async () => {
+        await Article.deleteOne(
+          { title: req.params.articleName }
+        );
+        res.send("Success!");
+      };
+      run().catch((err) => res.send(err));
+    } catch (err) {
+      res.send(err);
+    }
+  });
 
 app.listen("3000", () => {
   console.log("Listening on 3000");
